1000fc4d428d7e4cbf030bf02c7fdbea4a253800
Rodrigo <drigohenri@gmail.com>
2021-10-08 00:22:10 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,77 +1,15 @@
-import React, { useState, useEffect, Fragment } from 'react';
+import React from 'react';
 
-import { ThemeProvider } from "styled-components";
-import styled from "styled-components";
-import { lightTheme, darkTheme } from './theme/theme';
-import GlobalTheme from './theme/global'
-
-import { makeStyles } from '@material-ui/styles';
-
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import Brightness5Icon from '@mui/icons-material/Brightness5';
-import Brightness2Icon from '@mui/icons-material/Brightness2';
-
-import Index from './index/index'
-
-const useStyles = makeStyles({
-  
-  btnTheme: {
-      float:'right',
-      marginRight:25,
-      textAlign:'center'
-  }
-})
+import Index from './index/index'  
 
 function App() {
 
-  const classes = useStyles();
- 
-  const [theme, setTheme] = useState("light");
-
-  const toggleTheme = () => {
-    if (theme === "light") {
-      window.localStorage.setItem("theme", "dark");
-      setTheme("dark");
-    } else {
-      window.localStorage.setItem("theme", "light");
-      setTheme("light");
-    }
-  };
-
-  useEffect(() => {
-    const localTheme = window.localStorage.getItem("theme");
-    localTheme && setTheme(localTheme);
-  }, []);
-
   return (
     <div className="App">
-          <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
-          <Fragment >
-            <GlobalTheme />
-            <FormGroup className={classes.btnTheme} >
-                <Stack direction="row" spacing={1} alignItems="center">
-                  <Typography><Brightness5Icon fontSize="small" /></Typography>
-                    <FormControlLabel control={<Switch defaultChecked size="small" />}  onChange={toggleTheme}  label="" />
-                  <Typography><Brightness2Icon fontSize="small" /></Typography>
-                </Stack>
-              </FormGroup>
-          </Fragment>
-        </ThemeProvider>
         <Index />
     </div>
   );
 }
-
-const ButtonChange = styled.button`
-  width: 100px;
-  height: 40px;
-  margin-right: 20px;
-  border-radius: 10px;
-`;
 
 export default App;
 
--- src/header/header.js
+++ src/header/header.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect, Fragment }  from "react";
+
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from '../theme/theme';
+import GlobalTheme from '../theme/global';
+
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Brightness5Icon from '@mui/icons-material/Brightness5';
+import Brightness2Icon from '@mui/icons-material/Brightness2';
 
 import { makeStyles } from '@material-ui/styles';
 import Divider from '@mui/material/Divider';
 import Link from '@mui/material/Link';
 
-import logo from '../images/logoMybots.png'
+import logoLight from '../images/logoMybots_Light.png';
+import logoDark from '../images/logoMybots_Dark.png';
 
 const useStyles = makeStyles({
 
@@ -13,18 +26,54 @@ const useStyles = makeStyles({
     margin:30
 },
   divider:{
+    width:'100%',
     height:25,
+  },
+  btnTheme: {
+    float:'right',
+    textAlign:'center'
   }
 })
 
 export default function HeaderDoc() {
 
   const classes = useStyles();
+ 
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      window.localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    } else {
+      window.localStorage.setItem("theme", "light");
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
 
     return (
       <div className={classes.roots}>
-        <Link href=''><img className="logo" src={logo} style={{width: 220, height: 70}} /></Link>
-        <Divider className={classes.divider} />
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+          <Fragment >
+            <GlobalTheme />
+            <FormGroup className={classes.btnTheme} >
+                <Stack direction="row" spacing={1} alignItems="center">
+                  <Typography><Brightness2Icon sx={{marginBottom:-1}} fontSize="small" /></Typography>
+                    <FormControlLabel control={<Switch defaultChecked size="small" />} onChange={toggleTheme}  label="" />
+                  <Typography><Brightness5Icon sx={{marginBottom:-1}} fontSize="small" /></Typography>
+                </Stack>
+              </FormGroup>
+          </Fragment>
+        </ThemeProvider>
+          <Link href=''>
+            <img className="logo" src={theme === "light" ? logoLight : logoDark} style={{width: 220, height: 70}} />
+          </Link>
+        <Divider className={classes.divider} light={true}/>
       </div>
     );
   }
