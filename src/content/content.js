90f36ae92ec9897c1cdb0d9433ef530e5d23209b
Renato <renato.santos0611@gmail.com>
2021-10-15 17:55:59 -0300

    att

--- src/content/content.js
+++ src/content/content.js
@@ -17,11 +17,11 @@ export default function ContentDoc(props) {
     const classes = useStyles();
 
     return (
-        <div className="docContent">
+        <div id="docContent" className="docContent">
             {contentList.map((item) => {
                 return (
-                    <div className='root'>
-                        <div id={item.link}>
+                    <div>
+                        <div id={item.link} className='content'>
                             <h1>{item.title}</h1>
                             <p>Lorem ipsum dolor sit amet consectetur adipiscing elit id dictum iaculis fermentum feugiat venenatis mauris,consequat tellus ut lobortis blandit himenaeos mattis nisi est magnis sapien aptent. Sollicitudin augue suspendissedapibus non aliquet sodales accumsan nulla, porttitor ultricies molestie odio faucibus eget hendrerit tellus inceptos, massa risus torquent vitae erat maximus semper. Venenatis fames mattis semper tincidunt per eros finibusnon morbi pulvinar montes volutpat. Luctus lacinia porttitor natoque semper eleifend varius est id tincidunt quam, risus lobortis sit condimentum donec justo vel aptent ultrices augue, convallis maecenas quisque nisi ligula commodo pulvinar pretium nibh. Dis aliquet venenatis sit finibus commodo scelerisque lorem senectus, aenean id consectetur ut proin porta. Dignissim nullam maximus phasellus potenti consectetur sollicitudin quam elit feugiat, montes neque dictumst enim lectus maecenas aenean tincidunt odio ipsum, tempus magna finibus interdum consequat tristique hac porttitor.</p>
                         </div>
@@ -30,11 +30,11 @@ export default function ContentDoc(props) {
             })}
             <Link
                 activeClass='link'
-                to='root'
+                to='content'
                 spy={true}
                 smooth={true}
-                offset={-70}
-                duration={1000}
+                offset={-120}
+                duration={2000}
             >
                 <IconButton
                     sx={theme === "dark" ? { background: 'white', color: 'black', position: 'fixed', bottom: 10, right: 20, '&:hover': { background: 'red', color: 'white' } } : { background: 'black', color: 'white', position: 'fixed', bottom: 10, right: 20, '&:hover': { background: 'red', color: 'white' } }}>
--- src/header/header.js
+++ src/header/header.js
@@ -71,4 +71,3 @@ export default function HeaderDoc({ theme, toggleTheme, setTheme }) {
     </header>
   );
 }
-  
