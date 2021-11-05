6e215a3657555b6dd8b9e2d30dc01ad39b9a4177
Renato <renato.santos0611@gmail.com>
2021-11-05 17:58:24 -0300

    add

--- src/content/content.js
+++ src/content/content.js
@@ -1,13 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './content.css'
 import contentList from '../index/indexList.json'
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp'
 import Divider from '@mui/material/Divider'
-import ZoomInIcon from '@mui/icons-material/ZoomIn';
+import ZoomInIcon from '@mui/icons-material/ZoomIn'
+import IconButton from '@mui/material/IconButton'
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 import { Link } from 'react-scroll'
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+
 export default function ContentDoc () {
+  const [open, setOpen] = useState(false)
+
   return (
     <div id='doc-content' className='docContent'>
       <div className='iconButtonTop'>
@@ -31,18 +48,32 @@ export default function ContentDoc () {
             </h1>
             <div className='imageContent'>
               <img src={item.image} className='image' alt='imgContent' />
-                {console.log(item.image)}
-              <Link
-                activeClass='link'
-                to={item.image}
-                spy
-                smooth
-                offset={-120}
-                duration={2000}
-                hashSpy
+              {console.log(item.image)}
+
+              {/* <a  target="_blank" href={item.image}> */}
+           
+              <IconButton className='ZoomInIcon' onClick={() => setOpen(true)}>
+                <ZoomInIcon />
+              </IconButton>
+              <div >
+              <Modal
+                open={open}
+                onClose={() => setOpen(false)}
+                aria-labelledby='modal-modal-title'
+                sx={{background:'red'}}
 							>
-                <ZoomInIcon className='ZoomInIcon' />
-              </Link>
+                <Box sx={style}>
+                  <Typography id='modal-modal-title'>
+										Teste
+									</Typography>
+
+                  {/* <img src={item.image}  alt='imgContent' /> */}
+                  
+                </Box>
+              </Modal>
+              </div>
+
+              {/* </a> */}
             </div>
             <br />
             <p>
@@ -62,19 +93,19 @@ export default function ContentDoc () {
               return (
                 <div id={value.link} className='content'>
                   <h1>
-                    {value.title}
-                  </h1>
+                  {value.title}
+                </h1>
                   <br />
                   <Divider className='divider' />
                   <br />
                   <br />
                   <p>
-                    {value.text1}
-                  </p>
+                  {value.text1}
+                </p>
                   <br />
                   <p>
-                    {value.text2}
-                  </p>
+                  {value.text2}
+                </p>
                   <br />
                 </div>
               )
